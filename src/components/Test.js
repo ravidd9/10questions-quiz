1dd22e84e96fd85c30e2704001540acf6ef2a189
Ravid Cohen <ravidd9@gmail.com>
2019-05-18 19:02:55 +0300

    working with json file

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,7 +3,6 @@ import '../style/Test.css';
 import Question from './Question';
 import Navigation from './Navigation';
 import Grade from './Grade';
-// import data from '../data.JSON'
 
 
 class Test extends Component {
@@ -13,68 +12,7 @@ class Test extends Component {
             currentQuestion: 1,
             currentAnswer: null,
             answered: {},
-            questions: [
-                {
-                    "number": 1,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 2,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 3,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 4,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 5,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 6,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 7,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 8,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 9,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                },
-                {
-                    "number": 10,
-                    "question": "1+1",
-                    "correct": 4,
-                    "answers": ["4", "7", "9", "2"]
-                }
-            ]
+            questions: []
         }
     }
 
@@ -87,7 +25,6 @@ class Test extends Component {
 
     changeQuestion = updated => {
         if(this.state.answered[this.state.currentQuestion]){
-            // this.reMark()
             this.setState({isEmptyAnswer : false})
             this.setState({ currentQuestion: updated })
         }else if(updated < this.state.currentQuestion && updated > 0){
@@ -97,27 +34,24 @@ class Test extends Component {
         }
     }
 
-    // componentDidMount = () =>{
-    //     let data = 
-    //     // let questions = this.getQuestions()
-    //     this.setState({questions: data})
-    // }
+    componentDidMount = () =>{
+        let questions = this.getQuestions()
+        this.setState({questions})
+    }
 
-    // getQuestions = () => {
-    //     let questions = require('../data.JSON')
-    //     return questions.data
-    // }
-
-
+    getQuestions = () => {
+        let questions = require('../data.json')
+        return questions
+    }
 
     findCurrentQuestion = () => this.state.questions.find(q => q.number === this.state.currentQuestion)
 
     render() {
+        let questions = this.state.questions
         let question = this.findCurrentQuestion()
         return (
-
             <div id="test">
-                {this.state.currentQuestion > 0 ?
+                {this.state.currentQuestion > 0  && questions[0] ?
                     <div>
                         <Question
                             saveAnswer={this.saveAnswer}
@@ -129,7 +63,7 @@ class Test extends Component {
                             changeQuestion={this.changeQuestion} />
                         {this.state.isEmptyAnswer ? <div>Please enter your answer</div> : null}
                     </div> :
-                    <Grade questions={this.state.questions} answered={this.state.answered} />}
+                    <Grade questions={questions} answered={this.state.answered} />}
             </div>
         );
     }
